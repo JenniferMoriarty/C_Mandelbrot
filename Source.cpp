680b0fc6f02e77949ac16b2ef02e0a5b2af23bf1
Jen Moriarty <JMoriarty78@gmail.com>
2021-10-31 14:56:23 -0600

    Update Source.cpp
    updated to draw set black, added comments

--- Source.cpp
+++ Source.cpp
@@ -1,41 +1,54 @@
-#include <iostream>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include <complex>
-using namespace std;
-const int W = 800;
-const int H = 800;
+//mandelbrot set, coded by Dr. Mo, 2019 (updated 2021)
+
+#include <allegro5/allegro.h> //open source video game library
+#include <allegro5/allegro_primitives.h> //for drawing shapes like rects and circles and stuff
+#include <complex> //c++ library for complex numbers
+using namespace std; //needed for complex 
+
+//function declaration
 double mandelbrot(complex<double>c);
+
 int main() {
 
-	al_init();
-	al_init_primitives_addon();
-	ALLEGRO_DISPLAY* TIA = al_create_display(W, H);
+	al_init();//initalize allegro library
+	al_init_primitives_addon(); 
+
+	ALLEGRO_DISPLAY* screen = al_create_display(800, 800); //create game screen
+
+	//program variables
 	complex<double>c;
 	int num;
 
-	for (double t = -2; t < 2; t += .01)
-		for (double m = -2; m < 2; m += .01) {
-			cout << "loop";
-			c = complex<double>(t, m);
+	for (double x = -2; x < 2; x += .01)
+		for (double y = -2; y < 2; y += .01) {
+
+			c = complex<double>(x, y);
 			num = mandelbrot(c);
 
-			al_put_pixel(t * 100 + 400, m * 100 + 400, al_map_rgb(num * 200, num * 125, num / 255));
-			cout << "num is " << num << " at " << t * 100 + 400 << " " << m * 100 + 400 << endl;
-			al_flip_display();
+			//put_pixel() draws a single pixel to the screen at (x, y, color)
+			if(num == 80)
+				al_put_pixel((x * 100 + 400), (y * 100 + 400), al_map_rgb(0,0,0)); //draw inside black
+			else
+				al_put_pixel((x * 100 + 400), (y * 100 + 400), al_map_rgb(num * 200, num/255, num*25));
+
+			al_flip_display(); //flip what we just drawed to the screen
 		}
 	system("pause");
-	al_destroy_display(TIA);
+	al_destroy_display(screen);//release memory when done
 
 
 }
+
+//function definition-------------------------------------------------------------------------------------------
 double mandelbrot(complex<double>c) {
 	complex <double> z = 0;
 	int count = 0;
-	while (abs(z) < 2 && count < 80) {
-		z = z * z + c;
+
+	//run this loop 80 times and see if z*z+c gets bigger than 2 or stays smaller
+	while (abs(z) < 2 && count < 80) { 
+		z = z * z + c; //what happens when we times the number by itself and add c over and over again?
 
 		count++;
 	}
-	return count;
+	return count; //this represents how many times the loop ran (all 80 means it's most likely in the set)
 }
